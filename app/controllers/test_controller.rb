feb3c7ed1d1d4b5d25230a7263820af5c23eebf8
JackYu <codedogfish@github.com>
2015-05-28 23:20:55 +0800

    Test Apache+Passenger

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,6 +3,10 @@ class TestController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def index
+
+  end
+
   def show
   	require "rubygems"
 	require "bunny"
